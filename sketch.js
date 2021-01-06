a82d46823efb06b290476125fb0ee646a0ca2597
niconico-boop <75068808+niconico-boop@users.noreply.github.com>
2021-01-06 14:18:22 +1100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,50 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var engine, world;
+var paper1;
+var ground;
+var dustbin1;
+
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+	engine = Engine.create();
+	world = engine.world;
+
+	ground = new Ground(400, 680, 800, 30)
+	paper1 = new Paper(20, 670, 50, 50)
+	dustbin1 = new Dustbin(400, 650)
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+
+  keyPressed
+
+  ground.display();
+  paper1.display();
+  dustbin1.display();
+
+}
+
+
+
+
+
+
+
